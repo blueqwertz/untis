623809b325155dd3c8b3da897f5f8d79bd7861bc
blueqwertz <timlimlei@icloud.com>
2023-03-23 22:42:51 +0100

    change in cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "http://10.0.0.51:3001", "http://10.10.105.160:3001", "http://144.21.42.28:3001", "https://untis.bgpd.at", "https://webuntis.bgpd.at"]
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "http://10.0.0.51:3001", "http://10.10.105.160:3001", "http://10.0.0.93:3001", "https://untis.bgpd.at", "https://webuntis.bgpd.at"]
 
 module.exports = allowedOrigins
