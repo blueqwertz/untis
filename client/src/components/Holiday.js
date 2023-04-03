f9c7974b34e5c83069aa054801d3ba35caa18e29
blueqwertz <timlimlei@icloud.com>
2023-04-03 15:20:51 +0200

    made holiday text black/white

--- client/src/components/Holiday.js
+++ client/src/components/Holiday.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Holiday({ holidayName }) {
 	return (
-		<div className="flex-1 row-start-2 row-end-[14] bg-gray-400 dark:bg-slate-700 flex justify-center items-center overflow-hidden text-white">
+		<div className="flex-1 row-start-2 row-end-[14] bg-gray-400 dark:bg-slate-700 flex justify-center items-center overflow-hidden">
 			<div className="-rotate-90">{holidayName}</div>
 		</div>
 	)
