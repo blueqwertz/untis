ab4b234f0425746a4dc2bee5debfced64e67c38b
blueqwertz <timlimlei@icloud.com>
2023-04-04 12:44:52 +0200

    added server+client npm script

--- client/src/components/WeatherDisplay.js
+++ client/src/components/WeatherDisplay.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function WeatherDisplay({ weather, weatherIcons }) {
 	return weather ? (
-		<div className={`text-xxs gap-1 hidden @3xs:flex justify-center items-center text-gray-500 dark:text-slate-400`}>
+		<div className={`text-xxs gap-1 hidden @3xs:flex justify-center items-center text-gray-500 dark:text-slate-400`} title="Wetterdaten von open-meteo.com">
 			<span>{weather?.temp_max}°C</span>
 			<img className="w-3 h-3 drop-shadow-weather dark:drop-shadow-none" src={weatherIcons[weather?.code]} />
 		</div>
